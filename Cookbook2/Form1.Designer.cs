5935f777678f6ccb4d4d3474517448d483ab0206
andy-coding <32386192+andy-coding@users.noreply.github.com>
2017-12-17 12:44:56 +0000

    Add project files.

---
+++ Cookbook2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Cookbook2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstRecipes = new System.Windows.Forms.ListBox();
+            this.lstIngredients = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Recipe";
+            // 
+            // lstRecipes
+            // 
+            this.lstRecipes.FormattingEnabled = true;
+            this.lstRecipes.Location = new System.Drawing.Point(41, 36);
+            this.lstRecipes.Name = "lstRecipes";
+            this.lstRecipes.Size = new System.Drawing.Size(120, 160);
+            this.lstRecipes.TabIndex = 1;
+            this.lstRecipes.SelectedIndexChanged += new System.EventHandler(this.lstRecipes_SelectedIndexChanged);
+            // 
+            // lstIngredients
+            // 
+            this.lstIngredients.FormattingEnabled = true;
+            this.lstIngredients.Location = new System.Drawing.Point(186, 36);
+            this.lstIngredients.Name = "lstIngredients";
+            this.lstIngredients.Size = new System.Drawing.Size(120, 160);
+            this.lstIngredients.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Recipe Ingredients";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 547);
+            this.Controls.Add(this.lstIngredients);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstRecipes);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cookbook2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstRecipes;
+        private System.Windows.Forms.ListBox lstIngredients;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Cookbook2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Cookbook2
+{
+    public partial class Form1 : Form
+    {
+        string connectionString;
+        SqlConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            DisplayRecipe();
+
+        }
+
+        private void DisplayRecipe()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["Cookbook2.Properties.Settings.Cookbook2ConnectionString"].ConnectionString;
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Recipe", connection))
+            {
+                DataTable data = new DataTable();
+
+                adapter.Fill(data);
+
+                lstRecipes.DisplayMember = "Recipe";
+                lstRecipes.ValueMember = "RecipeId";
+                lstRecipes.DataSource = data;
+            }
+        }
+
+        private void DisplayRecipeIngredients()
+        {
+            string query = "SELECT a.Ingredient FROM Ingredient a INNER JOIN RecipeIngredient b " +
+                " ON (a.IngredientId = b.IngredientId) WHERE b.RecipeId = @RecipeId";
+
+            connectionString = ConfigurationManager.ConnectionStrings["Cookbook2.Properties.Settings.Cookbook2ConnectionString"].ConnectionString;
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@RecipeId", lstRecipes.SelectedValue);
+
+                DataTable data = new DataTable();
+
+                adapter.Fill(data);
+
+                lstIngredients.DisplayMember = "Ingredient";
+                lstIngredients.ValueMember = "IngredientId";
+                lstIngredients.DataSource = data;
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstRecipes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show(lstRecipes.SelectedValue.ToString());
+            DisplayRecipeIngredients();
+        }
+
+
+    }
+}
