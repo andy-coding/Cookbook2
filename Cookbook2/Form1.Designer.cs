c457719539cb09e6ce9988c2abb44df1d05a7a0c
andy-coding <32386192+andy-coding@users.noreply.github.com>
2017-12-18 13:29:53 +0000

    commit

--- Cookbook2/Form1.Designer.cs
+++ Cookbook2/Form1.Designer.cs
@@ -30,14 +30,20 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.lstRecipes = new System.Windows.Forms.ListBox();
-            this.lstIngredients = new System.Windows.Forms.ListBox();
+            this.lstRecipeIngredients = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnAddRecipe = new System.Windows.Forms.Button();
+            this.txtRecipeName = new System.Windows.Forms.TextBox();
+            this.btnUpdateRecipeName = new System.Windows.Forms.Button();
+            this.listAllIngredients = new System.Windows.Forms.ListBox();
+            this.btnAddToRecipe = new System.Windows.Forms.Button();
+            this.btnRemoveFromRecipe = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 20);
+            this.label1.Location = new System.Drawing.Point(31, 198);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 0;
@@ -46,35 +52,96 @@
             // lstRecipes
             // 
             this.lstRecipes.FormattingEnabled = true;
-            this.lstRecipes.Location = new System.Drawing.Point(41, 36);
+            this.lstRecipes.Location = new System.Drawing.Point(34, 214);
             this.lstRecipes.Name = "lstRecipes";
             this.lstRecipes.Size = new System.Drawing.Size(120, 160);
             this.lstRecipes.TabIndex = 1;
             this.lstRecipes.SelectedIndexChanged += new System.EventHandler(this.lstRecipes_SelectedIndexChanged);
             // 
-            // lstIngredients
+            // lstRecipeIngredients
             // 
-            this.lstIngredients.FormattingEnabled = true;
-            this.lstIngredients.Location = new System.Drawing.Point(186, 36);
-            this.lstIngredients.Name = "lstIngredients";
-            this.lstIngredients.Size = new System.Drawing.Size(120, 160);
-            this.lstIngredients.TabIndex = 3;
+            this.lstRecipeIngredients.FormattingEnabled = true;
+            this.lstRecipeIngredients.Location = new System.Drawing.Point(179, 214);
+            this.lstRecipeIngredients.Name = "lstRecipeIngredients";
+            this.lstRecipeIngredients.Size = new System.Drawing.Size(143, 160);
+            this.lstRecipeIngredients.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(183, 20);
+            this.label2.Location = new System.Drawing.Point(176, 198);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(96, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Recipe Ingredients";
             // 
+            // btnAddRecipe
+            // 
+            this.btnAddRecipe.Location = new System.Drawing.Point(45, 64);
+            this.btnAddRecipe.Name = "btnAddRecipe";
+            this.btnAddRecipe.Size = new System.Drawing.Size(75, 23);
+            this.btnAddRecipe.TabIndex = 4;
+            this.btnAddRecipe.Text = "Add Recipe";
+            this.btnAddRecipe.UseVisualStyleBackColor = true;
+            this.btnAddRecipe.Click += new System.EventHandler(this.btnAddRecipe_Click);
+            // 
+            // txtRecipeName
+            // 
+            this.txtRecipeName.Location = new System.Drawing.Point(45, 38);
+            this.txtRecipeName.Name = "txtRecipeName";
+            this.txtRecipeName.Size = new System.Drawing.Size(227, 20);
+            this.txtRecipeName.TabIndex = 5;
+            // 
+            // btnUpdateRecipeName
+            // 
+            this.btnUpdateRecipeName.Location = new System.Drawing.Point(137, 64);
+            this.btnUpdateRecipeName.Name = "btnUpdateRecipeName";
+            this.btnUpdateRecipeName.Size = new System.Drawing.Size(135, 23);
+            this.btnUpdateRecipeName.TabIndex = 6;
+            this.btnUpdateRecipeName.Text = "Update Reccipe Name";
+            this.btnUpdateRecipeName.UseVisualStyleBackColor = true;
+            this.btnUpdateRecipeName.Click += new System.EventHandler(this.btnUpdateRecipeName_Click);
+            // 
+            // listAllIngredients
+            // 
+            this.listAllIngredients.FormattingEnabled = true;
+            this.listAllIngredients.Location = new System.Drawing.Point(395, 214);
+            this.listAllIngredients.Name = "listAllIngredients";
+            this.listAllIngredients.Size = new System.Drawing.Size(154, 160);
+            this.listAllIngredients.TabIndex = 7;
+            // 
+            // btnAddToRecipe
+            // 
+            this.btnAddToRecipe.Location = new System.Drawing.Point(395, 380);
+            this.btnAddToRecipe.Name = "btnAddToRecipe";
+            this.btnAddToRecipe.Size = new System.Drawing.Size(154, 23);
+            this.btnAddToRecipe.TabIndex = 8;
+            this.btnAddToRecipe.Text = "Add To Reccipe";
+            this.btnAddToRecipe.UseVisualStyleBackColor = true;
+            this.btnAddToRecipe.Click += new System.EventHandler(this.btnAddToRecipe_Click);
+            // 
+            // btnRemoveFromRecipe
+            // 
+            this.btnRemoveFromRecipe.Location = new System.Drawing.Point(179, 380);
+            this.btnRemoveFromRecipe.Name = "btnRemoveFromRecipe";
+            this.btnRemoveFromRecipe.Size = new System.Drawing.Size(143, 23);
+            this.btnRemoveFromRecipe.TabIndex = 9;
+            this.btnRemoveFromRecipe.Text = "Remove From Reccipe";
+            this.btnRemoveFromRecipe.UseVisualStyleBackColor = true;
+            this.btnRemoveFromRecipe.Click += new System.EventHandler(this.btnRemoveFromRecipe_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(525, 547);
-            this.Controls.Add(this.lstIngredients);
+            this.ClientSize = new System.Drawing.Size(749, 547);
+            this.Controls.Add(this.btnRemoveFromRecipe);
+            this.Controls.Add(this.btnAddToRecipe);
+            this.Controls.Add(this.listAllIngredients);
+            this.Controls.Add(this.btnUpdateRecipeName);
+            this.Controls.Add(this.txtRecipeName);
+            this.Controls.Add(this.btnAddRecipe);
+            this.Controls.Add(this.lstRecipeIngredients);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lstRecipes);
             this.Controls.Add(this.label1);
@@ -91,8 +158,14 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox lstRecipes;
-        private System.Windows.Forms.ListBox lstIngredients;
+        private System.Windows.Forms.ListBox lstRecipeIngredients;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddRecipe;
+        private System.Windows.Forms.TextBox txtRecipeName;
+        private System.Windows.Forms.Button btnUpdateRecipeName;
+        private System.Windows.Forms.ListBox listAllIngredients;
+        private System.Windows.Forms.Button btnAddToRecipe;
+        private System.Windows.Forms.Button btnRemoveFromRecipe;
     }
 }
 
--- Cookbook2/Form1.cs
+++ Cookbook2/Form1.cs
@@ -14,7 +14,8 @@ namespace Cookbook2
 {
     public partial class Form1 : Form
     {
-        string connectionString;
+        string connectionString = ConfigurationManager.ConnectionStrings["Cookbook2.Properties.Settings.Cookbook2ConnectionString"].ConnectionString;
+
         SqlConnection connection;
 
         public Form1()
@@ -25,12 +26,18 @@ namespace Cookbook2
 
         }
 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
         private void DisplayRecipe()
         {
-            connectionString = ConfigurationManager.ConnectionStrings["Cookbook2.Properties.Settings.Cookbook2ConnectionString"].ConnectionString;
 
             using (connection = new SqlConnection(connectionString))
-            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Recipe", connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Recipe ORDER BY Recipe ", connection))
             {
                 DataTable data = new DataTable();
 
@@ -42,12 +49,17 @@ namespace Cookbook2
             }
         }
 
+        private void lstRecipes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show(lstRecipes.SelectedValue.ToString());
+            DisplayRecipeIngredients();
+            DisplayAllIngredients();
+        }
+
         private void DisplayRecipeIngredients()
         {
-            string query = "SELECT a.Ingredient FROM Ingredient a INNER JOIN RecipeIngredient b " +
-                " ON (a.IngredientId = b.IngredientId) WHERE b.RecipeId = @RecipeId";
-
-            connectionString = ConfigurationManager.ConnectionStrings["Cookbook2.Properties.Settings.Cookbook2ConnectionString"].ConnectionString;
+            string query = "SELECT a.Ingredient, a.IngredientId FROM Ingredient a INNER JOIN RecipeIngredient b " +
+                " ON (a.IngredientId = b.IngredientId) WHERE b.RecipeId = @RecipeId ORDER BY a.Ingredient";
 
             using (connection = new SqlConnection(connectionString))
             using (SqlCommand command = new SqlCommand(query, connection))
@@ -59,25 +71,103 @@ namespace Cookbook2
 
                 adapter.Fill(data);
 
-                lstIngredients.DisplayMember = "Ingredient";
-                lstIngredients.ValueMember = "IngredientId";
-                lstIngredients.DataSource = data;
+                lstRecipeIngredients.DisplayMember = "Ingredient";
+                lstRecipeIngredients.ValueMember = "IngredientId";
+                lstRecipeIngredients.DataSource = data;
+            }
+        }
+
+        private void DisplayAllIngredients()
+        {
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand("SELECT * FROM Ingredient", connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+
+                DataTable ingredientTable = new DataTable();
+                adapter.Fill(ingredientTable);
+                listAllIngredients.DisplayMember = "Ingredient";
+                listAllIngredients.ValueMember = "IngredientId";
+                listAllIngredients.DataSource = ingredientTable;
             }
         }
 
 
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnAddRecipe_Click(object sender, EventArgs e)
         {
+            string query = "INSERT INTO Recipe VALUES (@RecipeName)";
 
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                command.Parameters.AddWithValue("@RecipeName", txtRecipeName.Text);
+                command.ExecuteNonQuery();
+
+                DisplayRecipe();
+            }
         }
 
-        private void lstRecipes_SelectedIndexChanged(object sender, EventArgs e)
+        private void btnUpdateRecipeName_Click(object sender, EventArgs e)
         {
-            //MessageBox.Show(lstRecipes.SelectedValue.ToString());
-            DisplayRecipeIngredients();
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand("UPDATE Recipe SET Recipe = @RecipeName WHERE RecipeId = @RecipeId" , connection))
+            {
+                connection.Open();
+
+                command.Parameters.AddWithValue("@RecipeId", lstRecipes.SelectedValue);
+                command.Parameters.AddWithValue("@RecipeName", txtRecipeName.Text);
+
+                command.ExecuteNonQuery();
+
+                DisplayRecipe();
+            }
         }
 
 
+
+        private void btnAddToRecipe_Click(object sender, EventArgs e)
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand("INSERT INTO RecipeIngredient VALUES (@RecipeId, @IngredientId)", connection))
+            {
+                connection.Open();
+
+                command.Parameters.AddWithValue("@RecipeId", lstRecipes.SelectedValue);
+                command.Parameters.AddWithValue("@IngredientId", listAllIngredients.SelectedValue);
+
+              //  int selectedRecipeId = (Int32)lstRecipes.SelectedItem;
+
+                command.ExecuteScalar();
+
+                DisplayRecipeIngredients();
+
+              //  selectedRecipeId = 0;
+
+            }
+        }
+
+        private void btnRemoveFromRecipe_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE FROM RecipeIngredient WHERE RecipeId = @RecipeId AND IngredientId = @IngredientId";
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                command.Parameters.AddWithValue("@RecipeId", lstRecipes.SelectedValue);
+                command.Parameters.AddWithValue("@IngredientId", lstRecipeIngredients.SelectedValue);
+
+                command.ExecuteNonQuery();
+
+                DisplayRecipeIngredients();
+            }
+
+        }
     }
+    
 }
